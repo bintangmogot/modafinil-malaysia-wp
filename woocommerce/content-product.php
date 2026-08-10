df91de85640131f5ea87f3a58dee7353ebe5db98
bintangmogot <harunbintangmogot07@gmail.com>
2026-08-10 22:37:50 +0800

    style: revert image scale

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -38,7 +38,7 @@ $in_stock = $product->is_in_stock();
 
     <!-- Product Image -->
     <a href="<?= esc_url($link) ?>" class="relative block aspect-square overflow-hidden bg-surface">
-        <img src="<?= esc_url($image) ?>" alt="Buy <?= esc_attr($title) ?> online in Malaysia" loading="lazy" class="h-full w-full scale-110 object-contain transition-transform duration-500 group-hover:scale-125">
+        <img src="<?= esc_url($image) ?>" alt="Buy <?= esc_attr($title) ?> online in Malaysia" loading="lazy" class="h-full w-full object-contain transition-transform duration-500 group-hover:scale-110">
     </a>
 
     <!-- Product Info -->
