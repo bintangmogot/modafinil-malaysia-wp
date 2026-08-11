8560dcaf91651494a771ecd8f264511b559dfd32
bintangmogot <harunbintangmogot07@gmail.com>
2026-08-11 22:30:07 +0800

    Override .gform_fields grid row-gap and column-gap for closer field layout

---
+++ woocommerce/loop/loop-start.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/loop-start.php.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<div class="grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-6">
