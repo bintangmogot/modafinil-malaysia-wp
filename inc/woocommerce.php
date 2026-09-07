831041ac87bf1b04a932ac20a4375e9139177954
bintangmogot <harunbintangmogot07@gmail.com>
2026-09-07 18:13:24 +0800

    fix: add PHP filters to strip QRIS from button labels

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -149,3 +149,10 @@ function modmy_make_phone_mandatory_checkout($fields) {
     return $fields;
 }
 
+add_filter('woocommerce_product_single_add_to_cart_text', 'modmy_remove_qris_from_button', 9999);
+add_filter('woocommerce_product_add_to_cart_text', 'modmy_remove_qris_from_button', 9999);
+add_filter('woocommerce_order_button_text', 'modmy_remove_qris_from_button', 9999);
+function modmy_remove_qris_from_button($label) {
+    // Remove any occurrence of "QRIS" (with optional surrounding spaces and parentheses)
+    return preg_replace('/\s*\(?\s*QRIS\s*\)?/i', '', $label);
+}
